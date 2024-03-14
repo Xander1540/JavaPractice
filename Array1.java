dbfc6351ad3105defad8cd7b6187e89e23a23781
Xander1540 <adityaxxx2003@gmail.com>
2024-03-14 21:19:30 +0530

    4th commit

---
+++ Array1.java
@@ -0,0 +1,18 @@
+
+public class Array1 {
+    
+    public static void main(String[] args) {
+        
+        int marks[] = new int[10];
+         marks[0] = 90;
+         marks[1] = 91;
+         marks[2] = 92;
+         marks[3] = 93;
+         marks[4] = 94;
+
+         for(int i=0; i<=marks.length; i++){
+            System.out.println(marks[i]);
+         }
+         
+    }
+}
