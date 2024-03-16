2ca50abf24e2f5c490934f998e8c6c02c63919b7
Xander1540 <adityaxxx2003@gmail.com>
2024-03-16 22:35:29 +0530

    8th commit

--- ArrayUserInput.java
+++ ArrayUserInput.java
@@ -4,6 +4,7 @@ public class ArrayUserInput {
     
     public static void main(String[] args) {
         
+        @SuppressWarnings("resource")
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
